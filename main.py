e29efb0f2ea634bf82619063f9a4a1d7123c4664
Lautaro  Marin <marin.lautaro.ibqm4b@gmail.com>
2025-04-23 20:45:19 -0300

    correcion mensaje de consola

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import utilidades
 def main():
 
     primer_valor = int(input("Ingrese el primer valor binario\n"))
-    segundo_valor = int(input("Ingrese el primer valor binario\n"))
+    segundo_valor = int(input("Ingrese el segundo valor binario\n"))
 
     if not (utilidades.es_binario(primer_valor)) or not (utilidades.es_binario(segundo_valor)):
         print("Uno de los dos valores ingresados no es un valor binario, intentalo nuevamente\n")
